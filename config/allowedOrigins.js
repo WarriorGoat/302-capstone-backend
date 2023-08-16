240fbde9836fed42b5d3cb770b05b17331664916
Michael Spitz <michael.spitz@codeimmersives.com>
2023-08-16 13:19:58 -0400

    Upated Cors Origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
     "http://localhost:5002",
     "http://localhost:5000",
-    "https://www.builderlist.com",
-    "https://builderlist.com",
+    "https://www.yourbuilderlist.com",
+    "https://yourbuilderlist.com",
+    "https://www.buildersrolodex.com",
+    "https://buildersrolodex.com",
   ];
   
   module.exports = allowedOrigins;
